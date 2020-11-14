40f2637534e28b5c45ccd434ba01ede5940f040c
Daxton <s02048898067@gmail.com>
2020-11-15 06:49:29 +0800

    Fixed an issue where creepers were afraid of explosions and the health bar on their heads would not disappear

--- src/main/java/com/daxton/customdisplay/listener/EntityListener.java
+++ src/main/java/com/daxton/customdisplay/listener/EntityListener.java
@@ -1,18 +1,27 @@
 package com.daxton.customdisplay.listener;
 
+import com.daxton.customdisplay.CustomDisplay;
 import com.daxton.customdisplay.manager.BBDMapManager;
 import com.daxton.customdisplay.manager.HDMapManager;
 import com.daxton.customdisplay.task.bossbardisplay.AttackBossBar;
 import com.daxton.customdisplay.task.holographicdisplays.AnimalHD;
 import com.daxton.customdisplay.task.holographicdisplays.MonsterHD;
+import com.destroystokyo.paper.event.entity.CreeperIgniteEvent;
+import jdk.nashorn.internal.ir.Block;
+import org.bukkit.entity.Creeper;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.*;
+import org.bukkit.event.hanging.HangingBreakByEntityEvent;
 
 import java.util.UUID;
 
 public class EntityListener implements Listener {
+
+    private CustomDisplay cd = CustomDisplay.getCustomDisplay();
 
     @EventHandler
     public void onDeath(EntityDeathEvent e){
@@ -21,12 +30,11 @@ public class EntityListener implements Listener {
         if(targetUUID != null){
             AttackBossBar attackBossBar = BBDMapManager.getAttackBossBarMap().get(targetUUID);
             Player player = attackBossBar.getPlayer();
-            if(attackBossBar != null){
-                attackBossBar.getBossBar().removePlayer(player);
-                attackBossBar.getBukkitRunnable().cancel();
-                BBDMapManager.getAttackBossBarMap().remove(targetUUID);
-                BBDMapManager.getTargetAttackBossBarMap().remove(uuid);
-            }
+            attackBossBar.getBossBar().removePlayer(player);
+            attackBossBar.getBukkitRunnable().cancel();
+            BBDMapManager.getAttackBossBarMap().remove(targetUUID);
+            BBDMapManager.getTargetAttackBossBarMap().remove(uuid);
+
         }
         MonsterHD monsterHD = HDMapManager.getMonsterHDMap().get(uuid);
         if(monsterHD != null){
@@ -44,6 +52,36 @@ public class EntityListener implements Listener {
             animalHD.getBukkitRunnable().cancel();
             HDMapManager.getAnimalHDMap().remove(uuid);
         }
+    }
+
+    @EventHandler
+    public void onEntityExplode(EntityExplodeEvent event){
+        UUID uuid = event.getEntity().getUniqueId();
+        MonsterHD monsterHD = HDMapManager.getMonsterHDMap().get(uuid);
+        if(monsterHD != null){
+            monsterHD.getHologram().delete();
+            monsterHD.getHealthMap().clear();
+            monsterHD.getLocationMap().clear();
+            monsterHD.getBukkitRunnable().cancel();
+            HDMapManager.getMonsterHDMap().remove(uuid);
+        }
+    }
+
+    @EventHandler
+    public void onExplosionPrime(ExplosionPrimeEvent event){
+        final Entity entity = event.getEntity();
+        if(entity instanceof Creeper){
+            UUID uuid = entity.getUniqueId();
+            MonsterHD monsterHD = HDMapManager.getMonsterHDMap().get(uuid);
+            if(monsterHD != null){
+                monsterHD.getHologram().delete();
+                monsterHD.getHealthMap().clear();
+                monsterHD.getLocationMap().clear();
+                monsterHD.getBukkitRunnable().cancel();
+                HDMapManager.getMonsterHDMap().remove(uuid);
+            }
+        }
+
     }
 
 }
